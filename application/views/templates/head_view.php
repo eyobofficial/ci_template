0d95f1e0ee88e4544d35c079fb0e0b5705313abe
eyob tariku <eyobtariku@gmail.com>
2016-09-05 18:34:01 +0300

    Create head_view.php & foot_view.php template view files

---
+++ application/views/templates/head_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Template</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="author" content="Eyob Tariku Sintaro">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/main.css'); ?>">
+</head>
+<body>
+<div id="wrapper">

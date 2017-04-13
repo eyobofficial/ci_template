8bcc441fd42723314fc48bb9239454ff1042be43
eyob tariku <eyobtariku@gmail.com>
2017-04-13 21:44:21 +0300

    Add base MY_Controller and MY_Model class to application/core

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,43 +1,32 @@
-<?php defined('BASEPATH') OR ('No direct script allowed');
-
-/**
- * Base controller to extend future controllers
- * 
- * @package   
- * @copyright
- * @link
- * @author        Eyob Tariku <eyobtariku@gmail.com>
- */
+<?php
 
 class MY_Controller extends CI_Controller {
 
-	/**
-	 * Property to pass all data
-	 */
 	public $data = array();
 
-
-	/**
-	 * Constructor Method
-	 */
 	public function __construct(){
 		parent::__construct();
-
+		
 	}
 
 
-	/**
-	 * Method to render page from template_view
-	 */
-	public function render($main_vew = NULL){
-		if( ! is_null($main_view) && file_exists(APPPATH . "views/" . $main_view . ".php")){
 
-			$template_path = 'templates/template_view';
-			$this->data['main_view'] = $main_view;
-
-			$this->load->view($template_path, $this->data);
-		}else{
-			show_404();
-		}
+	public function render($main_view, $template = "main_template_view"){
+		$this->data['main_view'] = $main_view;
+		$this->load->view($template, $this->data);
 	}
-}
+
+
+
+	public function render_account($main_view){
+		$template = 'account/account_template_view';
+		$this->render($main_view, $template);
+	}
+
+
+
+
+
+
+
+} /*** End MY_Controller ****/
